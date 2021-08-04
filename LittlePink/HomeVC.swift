3281d6de49e92503b087666f0c3b1618f18b59f9
lichunfei <lichunfeigood@gmail.com>
2021-08-04 23:38:38 +0800

    更新提交barView

---
+++ LittlePink/HomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  HomeVC.swift
+//  LittlePink
+//
+//  Created by lichunfei on 2021/8/4.
+//
+
+import UIKit
+import XLPagerTabStrip
+class HomeVC: ButtonBarPagerTabStripViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
